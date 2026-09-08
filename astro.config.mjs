ff926d665da0315905a68c03d64574e3b2baf7ef
Chritina <106293659+lna927438@users.noreply.github.com>
2026-09-08 13:18:50 +0800

    Configure GitHub Pages preview base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  site: "https://lna927438.github.io",
+  base: "/nolan-investment-office"
+});
